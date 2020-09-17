6d7c15687716d73a16e1c010880b922180f33bf2
light <865460105@qq.com>
2020-09-18 00:46:47 +0800

    add header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import '../css/Header.css';
+
+class Header extends Component {
+  render () {
+    return (
+      <header id="title">
+        todos
+      </header>)
+  }
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import React from 'react';
 import './css/index.css';
 import Todo from './components/Todo'
 import Footer from './components/Footer'
+import Header from './components/Header'
 
 let destination = document.querySelector('#container')
 
 ReactDOM.render(
   <div>
+    <Header></Header>
     <Todo></Todo>
     <Footer></Footer>
   </div>, destination
